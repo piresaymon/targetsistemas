c3312f57d8c23ab480927498c45130e356df8c61
Saymon Pires <piresaymon@gmail.com>
2023-03-08 22:00:48 -0300

    exercício 5

---
+++ exercicio5.py
@@ -0,0 +1,8 @@
+# recebe a string do usuário
+string = input("Digite uma palavra ou frase: ")
+
+# cria uma nova string vazia
+nova_string = ""
+
+# percorre a string original de trás para frente
+for i in range(len(string
